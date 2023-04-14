148722c47e5e6afe3b72043af8d2a83c0fad27d1
Basil <basilientos@gmail.com>
2023-04-14 10:55:53 +0600

    Реализован вывод контактов на главную страницу

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Setting;
 use App\Services\CategoryService;
+use App\Services\SettingService;
 use Illuminate\Http\Request;
 
 class MainPageController extends Controller
 {
-    public function index(CategoryService $categoryService)
+    public function index(CategoryService $categoryService, SettingService $settingService)
     {
-        return view('home')->with('categories', $categoryService->getCategories());
+        return view('home')
+                ->with('categories', $categoryService->getCategories())
+                ->with('phone', $settingService->getSettingByCode(Setting::CODE_PHONE))
+                ->with('address', $settingService->getSettingByCode(Setting::CODE_ADDRESS))
+                ->with('email', $settingService->getSettingByCode(Setting::CODE_EMAIL));
     }
 }
---
+++ app/Models/Setting.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $type
+ * @property string $code
+ * @property string $value
+ * @property string $name
+*/
+class Setting extends Model
+{
+    use HasFactory;
+
+    public const CODE_PHONE = 'phone';
+    public const CODE_ADDRESS = 'address';
+    public const CODE_EMAIL = 'email';
+
+    protected $guarded = ['id'];
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     * @return Setting
+     */
+    public function setType(string $type): Setting
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param string $code
+     * @return Setting
+     */
+    public function setCode(string $code): Setting
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     * @return Setting
+     */
+    public function setValue(string $value): Setting
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Setting
+     */
+    public function setName(string $name): Setting
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
