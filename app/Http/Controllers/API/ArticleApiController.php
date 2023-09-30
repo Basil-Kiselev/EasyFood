423d5ff9fd190e9aaf8eaf31938be71b4f83401e
Basil <basilientos@gmail.com>
2023-09-30 15:57:45 +0600

    Реализован api для получения категорий статей

--- app/Http/Controllers/API/ArticleApiController.php
+++ app/Http/Controllers/API/ArticleApiController.php
@@ -4,16 +4,23 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ArticlesApiRequest;
+use App\Http\Resources\ArticleCategoryResource;
 use App\Http\Resources\ArticleShortResource;
 use App\Services\BlogService;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ArticleApiController extends Controller
 {
-    public function getArticles(ArticlesApiRequest $request, BlogService $service)
+    public function getArticles(ArticlesApiRequest $request, BlogService $service): AnonymousResourceCollection
     {
         $type = $request->getType();
         $category = $request->getCategory();
 
         return ArticleShortResource::collection($service->getArticlesCollection($category, $type));
+    }
+
+    public function getArticleCategories(BlogService $service): AnonymousResourceCollection
+    {
+        return ArticleCategoryResource::collection($service->getArticleCategories());
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,8 @@ Route::get('/products/{alias}', [ProductApiController::class, 'getProduct']);
 
 Route::get('/articles', [ArticleApiController::class, 'getArticles']);
 
+Route::get('/article-categories', [ArticleApiController::class, 'getArticleCategories']);
+
 Route::post('/subscribe',[NewsletterSubscribeController::class, 'subscribeNewEmail']);
 
 Route::post('/cart', [CartController::class, 'applyCoupon']);
