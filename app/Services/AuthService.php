e4c692fec753f97105bbae34e5a657f0e37cacd0
Basil <basilientos@gmail.com>
2023-10-12 17:45:51 +0600

    Изменены ответы методов логина и регистрации

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -7,13 +7,12 @@ use App\Models\User;
 use App\Services\DTO\RegistrationNewUserDTO;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Session;
 
 class AuthService
 {
     public const AUTH_API_TRUE = true;
 
-    public function register(RegistrationNewUserDTO $userData, string|null $fingerprint = null): bool
+    public function register(RegistrationNewUserDTO $userData, ?string $fingerprint = null): bool
     {
         $newUser = User::query()->create([
             'name' => $userData->getName(),
@@ -29,7 +28,7 @@ class AuthService
         return true;
     }
 
-    public function login(string $email, string $password, string|null $fingerprint = null): bool
+    public function login(string $email, string $password, ?string $fingerprint = null): bool
     {
         $credential = [
           'email' => $email,
@@ -44,7 +43,7 @@ class AuthService
         return Auth::check();
     }
 
-    public function loginOrRegister(RegistrationNewUserDTO $userData, string|null $fingerprint = null, bool|null $api = null): string|bool
+    public function loginOrRegister(RegistrationNewUserDTO $userData, ?string $fingerprint = null, ?bool $api = null): string|bool
     {
         $email = $userData->getEmail();
         $user = User::query()->where('email', $email)->first();
@@ -71,7 +70,7 @@ class AuthService
         }
     }
 
-    public function loginApi(string $email, string $password, string|null $fingerprint = null): bool|string
+    public function loginApi(string $email, string $password, ?string $fingerprint = null): bool|string
     {
         $credential = [
             'email' => $email,
@@ -95,7 +94,7 @@ class AuthService
         }
     }
 
-    public function registerApi(RegistrationNewUserDTO $userData): bool|string
+    public function registerApi(RegistrationNewUserDTO $userData): string
     {
         /** @var User $newUser */
         $newUser = User::query()->create([
