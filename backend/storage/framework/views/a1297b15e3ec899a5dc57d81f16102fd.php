f9904b25a6f404140d401ace2bc061522d92b30a
Basil-Kiselev <basiliento@gmail.com>
2023-11-11 11:23:47 +0600

    Из репозитория удалены ошибочно добавленные файлы

--- backend/storage/framework/views/a1297b15e3ec899a5dc57d81f16102fd.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    <li><i class="fa fa-envelope"><?php echo e($email); ?></i></li>
-    <li>Бесплатная доставка от <?php echo e($freeDelivery); ?> ₸</li>
-</ul>
-<?php /**PATH /var/www/html/resources/views/blocks/header/info.blade.php ENDPATH**/ ?>
